8cb064cb6d5536d95a39930f1ec27f29ee2bab71
Adrian Valdes <adrianvaldesrodriguez@gmail.com>
2024-07-26 17:54:29 +0200

    Ejercicios de clase

---
+++ src/main/Main.java
@@ -0,0 +1,234 @@
+package main;
+import utilidades.Entrada;
+
+public class Main {
+
+	public static void main(String[] args) {
+		/*System.out.println("Hola mundo");
+		// declara tres variables con el valor que tu elijas y muestrala por pantalla
+		
+		int miEdad = 34;
+		System.out.println(miEdad);
+				
+		float misAños = 36;
+		System.out.println(misAños);
+		
+		boolean miTrue = true;
+		System.out.println(miTrue);
+		
+		String miString = "Mi cadena";
+		System.out.println(miString);
+		
+				
+		
+		
+		int cantidadPeras = 4;
+		System.out.println(cantidadPeras);
+		
+		boolean heComidoHoy = true;
+		System.out.println(heComidoHoy);
+		
+		System.out.print("Mi nombre es: ");
+		
+		String miNombre = "Adrian";
+		System.out.print(miNombre);*/
+		
+		/*int num1 = 7;
+		int num2 = 6;
+		System.out.println(num1 < num2); // False 
+		System.out.println(num1 > num2);  // Verdadero		
+		
+		num2 = 7; 
+				
+		System.out.println(num1 >= num2);*/
+		
+		// EJERCICIO NUM 1
+		
+		/*int lado1 = 5;
+		int lado2 = 8;
+		int suma;
+		suma = lado1 + lado2;
+		int multiplicacion;
+		multiplicacion = suma * 2;
+		System.out.println("El perímetro mide " + multiplicacion + " metros");*/
+		
+		
+		// EJERCICIO NUM 2 
+		
+		/*int alumno1 = 14;
+		int alumno2 = 19;
+		
+		if(alumno1 < 18){
+			System.out.println("El alumno es menor de edad");
+		} else { 
+			System.out.println("El alumno es mayor de edad");
+		}
+		
+		if(alumno2 > 18) {
+			System.out.println("El alumno es mayor de edad");
+		} else {
+			System.out.println("El alumno es menor de edad");
+		}*/
+		//EJERCICIO NUM3 
+		
+		/*int num1 = -5;
+		int num2 = 4;
+		int num3 = 0;
+		
+		if(num1 < 0){
+			System.out.println("El numero es negativo");
+		} else {
+			System.out.println("El numero es positivo o cero");
+		}
+		
+		if(num2 > 0) {
+			System.out.println("El numero es positivo");
+		} else { 
+			System.out.println("El numero es negativo o cero");
+		}
+	    
+		if(num3 < 0) {
+			System.out.println("el numero es menor que cero");
+		} else if(num3== 0){
+			System.out.println("El numero es cero");
+		} else { 
+			System.out.println("El numero es mayor que cero");
+			
+		}*/
+		
+		/*// EJERCICIO NUM 4 
+		
+		int num1 = 123;
+		int num2 = 356;
+		if (num1 % 2 == 0) {
+			System.out.println(" El numero es par");
+		} else { 
+			System.out.println("El numero es impar");
+		}
+		if (num2 % 2 == 0) {
+			System.out.println("El numero es par");
+		} else {
+			System.out.println("El numero es impar");
+		}*/
+		
+		
+		/*System.out.print("Introduce una cadena: ");
+        String cadena = Entrada.introducirCadena();
+        System.out.println(cadena);*/
+	
+		
+		
+		
+		/*// Ejercicio1 modificado
+		
+		System.out.print("Introduce un lado de la balsa (mt): ");
+		int lado1 = Entrada.introducirEntero();
+		System.out.print("Introduce el otro lado de la balsa (mt): ");
+		int lado2 = Entrada.introducirEntero();
+		int suma;
+		suma = lado1 + lado2;
+		int multiplicacion;
+		multiplicacion = suma * 2;
+		System.out.println("El perímetro mide " + multiplicacion + " metros");*/
+		
+		/*//EJERCICIO 2 
+		
+		System.out.print("Introduce la edad del alumno1: ");
+		int edad1 = Entrada.introducirEntero();
+		if(edad1 < 18) {
+			System.out.println("El alumno es menor de edad");
+		} else { 
+				System.out.println("El alumno es mayor de edad");
+		}
+		System.out.print("Introduce la edad del alumno2: ");
+		int edad2 = Entrada.introducirEntero();
+		if(edad1 < 18) {
+			System.out.print("El alumno es menor de edad");
+		} else { 
+				System.out.print("El alumno es mayor de edad");
+		}*/
+		
+		//EJERCICIO 3 
+		
+		
+		/*System.out.print("Introduce el primer número: ");
+		int num1 = Entrada.introducirEntero();
+		if(num1 < 0) {
+			System.out.println("El número es negativo");
+		} else if(num1 == 0) {
+			System.out.println("El numero es cero");
+		} else {
+			System.out.println("El numero es positivo");
+		}
+	
+		System.out.print("Introduce el segundo número: ");
+		int num2 = Entrada.introducirEntero();
+		if(num2 < 0) {
+			System.out.println("El número es negativo");
+		} else if(num2 == 0) {
+			System.out.println("El numero es cero");
+		} else {
+			System.out.println("El numero es positivo");
+		}
+		System.out.print("Introduce el tercer número: ");
+		int num3 = Entrada.introducirEntero();
+		if(num3 < 0) {
+			System.out.println("El número es negativo");
+		} else if(num3 == 0) {
+			System.out.println("El numero es cero");
+		} else {
+			System.out.println("El numero es positivo");
+		}*/
+		
+		// EJERCICIO 4
+		
+		System.out.print("Introduce el primer numero: ");
+		int num1 = Entrada.introducirEntero();
+		if (num1 % 2 == 0) {
+			System.out.println(" El numero es par");
+		} else { 
+			System.out.println("El numero es impar");
+		}
+		System.out.print("Introduce el segundo numero: ");
+		int num2 = Entrada.introducirEntero();
+		if (num2 % 2 == 0) {
+			System.out.println("El numero es par");
+		} else {
+			System.out.println("El numero es impar");
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+				
+		
+		
+		
+		
+	}
+
+}
